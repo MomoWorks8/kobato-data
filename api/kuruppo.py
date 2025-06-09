75a967e47e40681d771210dbd2063e652c0075b0
MomoWorks8 <tiny.angel.lily@gmail.com>
2025-06-09 09:25:04 +0700

    Update and rename kuruppo.py to kuruppo.js

--- api/kuruppo.py
+++
@@ -1,9 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write("くるっぽ〜！Hello from Vercel🐦".encode("utf-8"))
-        return
